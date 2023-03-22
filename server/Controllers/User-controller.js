eea1fa1d82a1618d589183c4390d410767b392f1
prabalLuniva <126548777+prabalLuniva@users.noreply.github.com>
2023-03-22 15:50:04 +0545

    'change'

--- server/Controllers/User-controller.js
+++ server/Controllers/User-controller.js
@@ -1,15 +1,31 @@
 const User = require("../Models/User");
+const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const addNewUser = async (req, res) => {
   const { username, password } = req.body;
+  console.log(username, password);
+
+  const salt = await bycrypt.genSalt(10);
+
+  const hashedPassword = await bycrypt.hash(password, salt);
 
   try {
     const user = new User({
       username,
-      password,
+      password: hashedPassword,
     });
     await user.save();
-    return res.status(201).json({ msg: "User registered successfully" });
+    const token = jwt.sign(
+      { userId: user._id, username: user.username },
+      "secret",
+      {
+        expiresIn: "30d",
+      }
+    );
+    return res
+      .status(201)
+      .json({ username: { name: user.username }, token: token });
   } catch (error) {
     console.log(error);
     return res.status(400).json({ msg: "something went wrong" });
@@ -23,7 +39,7 @@ const getUser = async (req, res) => {
     console.log(result);
     if (!result) {
       res.status(404).json({ msg: "No user found" });
-    } else if (result.password === password) {
+    } else if (await bycrypt.compare(password, result.password)) {
       res.status(200).json({ msg: "successful", successMsg: true });
     } else {
       res.status(404).json({ msg: "wrong username or password" });
